3f615d8cfe1ad6b51700deb01e82197553db53d9
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-06-14 20:34:55 +0600

    🛠️📕completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { AppContainer } from "./style";
 
 function App() {
+  const [isAnunul, setIsAnnul] = useState(false);
+
+  const Annual = [
+    {
+      title: "Basic",
+      cost: "199.99",
+      benefits: ["500 GB Storage", "2 Users Allowed", "Send up to 3 GB"],
+    },
+    {
+      title: "Professional",
+      cost: "249.99",
+      benefits: ["1 TB Storage", "5 Users Allowed", "Send up to 10 GB"],
+    },
+    {
+      title: "Master",
+      cost: "399.99",
+      benefits: ["2 TB Storage", "10 Users Allowed", "Send up to 20 GB"],
+    },
+  ];
+
+  const Monthly = [
+    {
+      title: "Basic",
+      cost: "19.99",
+      benefits: ["500 GB Storage", "2 Users Allowed", "Send up to 3 GB"],
+    },
+    {
+      title: "Professional",
+      cost: "24.99",
+      benefits: ["1 TB Storage", "5 Users Allowed", "Send up to 10 GB"],
+    },
+    {
+      title: "Master",
+      cost: "49.99",
+      benefits: ["2 TB Storage", "10 Users Allowed", "Send up to 20 GB"],
+    },
+  ];
+
+  const OPTIONS = isAnunul ? Annual : Monthly;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <AppContainer>
+      <h1>Our Pricing</h1>
+      <div className="option_switcher">
+        <p>Annually</p>
+        <div
+          className={`toggler ${isAnunul ? "anu" : "mon"}`}
+          onClick={() => setIsAnnul(!isAnunul)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="toggler_circle"></div>
+        </div>
+        <p>Monthly</p>
+      </div>
+      <div className="options">
+        {React.Children.toArray(
+          OPTIONS.map((data) => (
+            <div className="option">
+              <div className="option_title">{data.title}</div>
+              <div className="option_cost">
+                <span>$</span>
+                {data.cost}
+              </div>
+              {React.Children.toArray(
+                data.benefits.map((d) => (
+                  <>
+                    <div className="option_line"></div>
+                    <div className="option_benefit">{d}</div>
+                  </>
+                ))
+              )}
+              <div className="option_line"></div>
+              <div className="option_button">Learn More</div>
+            </div>
+          ))
+        )}
+      </div>
+    </AppContainer>
   );
 }
 
